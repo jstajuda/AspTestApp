3fa7247c02881ed45ec2b073fa9cbfcfd9614d6f
Jaroslaw Stajuda <jaroslaw.stajuda@globallogic.com>
2020-01-06 14:58:35 +0100

    Add project files.

---
+++ AspTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
